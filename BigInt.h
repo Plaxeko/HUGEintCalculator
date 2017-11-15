6f96f2bd88793f90557ecc5cbacdc87c41927925
Amir Torabi <Torabi10@gmail.com>
2017-11-15 12:00:01 -0600

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -4,13 +4,12 @@ class BigInt
 {
 private:   
     bool verbose;
-
+    
 public:
     void put_verbose(bool verb);
     std::string copy_next(int *index, std::string in);
     std::string add(std::string in,bool printResult);
     std::string multiply(std::string in, bool printResult);
     std::string exponent(std::string in);
-
-
+    
 };
