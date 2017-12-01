cb162e51924eab972f5895118e25389cccb8085f
Amir Torabi <Torabi10@gmail.com>
2017-12-01 09:44:20 -0600

    Update BigInt.h

--- BigInt.h
+++ BigInt.h
@@ -1,37 +1,19 @@
 #pragma once
-<<<<<<< HEAD
 #include <string>
 using namespace std;
 namespace mesa
 {
 	class BigInt
 	{
-	private:
+	private:   
 		bool verbose;
-
+		
 	public:
 		void put_verbose(bool verb);
 		std::string copy_next(int *index, std::string in);
 		std::string add(string A, string B, bool printResult);
 		std::string multiply(string A, string B, bool printResult);
 		std::string exponent(string base, string exp,  bool printResult);
-
+		
 	};
 }
-
-=======
-
-class BigInt
-{
-private:   
-    bool verbose;
-    
-public:
-    void put_verbose(bool verb);
-    std::string copy_next(int *index, std::string in);
-    std::string add(std::string in,bool printResult);
-    std::string multiply(std::string in, bool printResult);
-    std::string exponent(std::string in);
-    
-};
->>>>>>> origin/master
